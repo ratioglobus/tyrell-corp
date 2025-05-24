ec824829111406d34065411066e5d3049eecf7e3
Ivan <dovakin.rs@gmail.com>
2025-05-24 20:53:27 +0300

    Add root route for Render health check

--- tyrell-backend/server.js
+++ tyrell-backend/server.js
@@ -7,11 +7,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error(err));
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error(err));
 
 app.use('/api/replicants', require('./routes/replicants'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('Tyrell Backend is running');
+});
